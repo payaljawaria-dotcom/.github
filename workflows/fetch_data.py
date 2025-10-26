346995958d3c268515efe6be0cdfee5bfa3aceb7
payaljawaria-dotcom <payal.jawaria@gmail.com>
2025-10-26 15:20:41 +0530

    Update fetch_data.py

--- workflows/fetch_data.py
+++ workflows/fetch_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
-import os
-print("Current directory:", os.getcwd())
-print("Files here:", os.listdir())
-print("It is here")
-#df = pd.read_excel('Int B.Tech_M.tech.xlsx')
-#print(df.head())
+# import os
+# print("Current directory:", os.getcwd())
+# print("Files here:", os.listdir())
+# print("It is here")
+df = pd.read_excel('Int B.Tech_M.tech.xlsx')
+print(df.head())
