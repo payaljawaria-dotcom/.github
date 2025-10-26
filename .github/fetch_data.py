e14d97bd0392a3c49e53cc98695818df765a2c4a
payaljawaria-dotcom <payal.jawaria@gmail.com>
2025-10-26 18:12:41 +0530

    Delete .github directory

--- .github/fetch_data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import os
-print("Current directory:", os.getcwd())
-print("Files here:", os.listdir())
-print("it is heeeerreeee")
-#df = pd.read_excel('workflows/Int B.Tech_M.tech.xlsx')
-#print(df.head())
--- .github/workflows/fetch_data.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import os
-print("Current directory:", os.getcwd())
-print("Files here:", os.listdir())
-
